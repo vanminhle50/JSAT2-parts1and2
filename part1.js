9fc243e93468f862cdcad2c198d6db4b5067730f
vanminhle50 <vle50@student.holmesglen.edu.au>
2025-03-24 11:02:14 +1100

    Update Part 1: Step 01
    Part 1: Data structure and algorithm – simple data type
    
    Build an array with the following numbers.
    11, 5, 8, 3, 25, 16, 31, 45, 14, 20

---
+++ part1.js
@@ -0,0 +1,21 @@
+// ---------------------------------------------------------------------
+// Part 1: Data structures and algorithms – simple data types.
+// ---------------------------------------------------------------------
+// Certificate IV in Information Technology
+// Holmesglen institute, VIC , Australia
+// Student Name: Van Minh Le
+// Student ID: 100693330
+// ---------------------------------------------------------------------
+
+// Step 1: Create an array with the given numbers
+// Define a array of numbers
+/**
+ * Sorts the input array in ascending order.
+ * @param {Array} inputArray - The array is build wwith the following numbers 11, 5, 8, 3, 25, 16, 31, 45, 14, 20.
+ */
+let inputArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+
+// Display the original array in HTML element with id "original-array"
+document.getElementById('original-array').innerHTML = `<span class="bold note"> ${inputArray} </span>`;
+//Print inputArray to the screen.
+console.log(`The inputArray ${inputArray}`);
